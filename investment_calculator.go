bf4138c78bbfc60281820d48e5ab9d449c925717
Levi McDonough <levimcdonough@gmail.com>
2025-09-27 19:12:07 -0400

    Refactor investment calculator to use user input for investment amount and fix future value calculation

--- investment_calculator.go
+++ investment_calculator.go
@@ -1,9 +1,20 @@
 package main
 
-func main() {
-	var investmentAmount = 100
-	var expedReturnRate = 5.5
-	var years = 10
+import (
+	"fmt"
+	"math"
+)
 
-	var futureValue = investmentAmount*(1+expectedReturnRate/100) ^ years
+func main() {
+	const inflationRate = 2.5
+	var investmentAmount float64
+	years := 10.0
+	expectedReturnRate := 5.5
+
+	fmt.Scan(&investmentAmount)
+
+	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
